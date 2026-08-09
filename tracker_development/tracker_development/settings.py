470e2523a8da34479eab5b36583f2c296fec930b
Frank Thompson <frankthompson@gmail.com>
2026-08-08 22:50:21 -0700

    trying postgres

--- tracker_development/tracker_development/settings.py
+++ tracker_development/tracker_development/settings.py
@@ -89,9 +89,13 @@ WSGI_APPLICATION = 'tracker_development.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": os.environ["PGPORT"],
     }
 }
 

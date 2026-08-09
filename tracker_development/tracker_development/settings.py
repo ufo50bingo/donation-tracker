f9a589522ff00dca7bbfaa04f075ffa9c33c6bd3
Frank Thompson <frankthompson@gmail.com>
2026-08-09 12:26:36 -0700

    10 min donate timeout instead of 1 min

--- tracker_development/tracker_development/settings.py
+++ tracker_development/tracker_development/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +152,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = 'tracker_development.routing.application'
 CHANNEL_LAYERS = {'default': {'BACKEND': 'channels.layers.InMemoryChannelLayer'}}
 PAYPAL_TEST = True
+TRACKER_PAYPAL_MAX_DONATE_AGE = 600

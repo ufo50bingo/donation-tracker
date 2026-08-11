f0e14a10235d302a87988e500e49d57a638b6869
Frank Thompson <frankthompson@gmail.com>
2026-08-10 17:09:29 -0700

    try fixing sockets

--- tracker_development/tracker_development/asgi.py
+++ tracker_development/tracker_development/asgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracker_development.settings')
 
-application = get_asgi_application()
+from .routing import application

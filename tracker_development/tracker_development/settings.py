2a2c6a6fba6d32462c4a6f67aa1af58ab0787404
Frank Thompson <frankthompson@gmail.com>
2026-08-11 22:29:10 -0700

    debug mode off

--- tracker_development/tracker_development/settings.py
+++ tracker_development/tracker_development/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
   'donation-tracker-production-f333.up.railway.app',
